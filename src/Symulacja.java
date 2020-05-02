ce041768b8dbc92766c88d7690df8efc71b6a4de
Docent <idontknnow5325@gmail.com>
2020-05-02 10:52:06 +0200

    Projekt

--- src/Symulacja.java
+++ src/Symulacja.java
@@ -1,5 +1,9 @@
 public class Symulacja {
     public static void main(String[] args){
-
+            System.out.println("Generuję");
+            Student Janusz= new Student();
+            System.out.println(Janusz.getIntelligence());
+            System.out.println(Janusz.getCleverness());
+            System.out.println(Janusz.getLaziness());
     }
 }
